e4297194e4af3af763c3deed8a46862304033c16
riadulislam111 <riadulislam.bd.pabna@gmail.com>
2026-08-14 04:50:05 +0600

    added all files updats

---
+++ practic.js
@@ -0,0 +1,86 @@
+"use strict";
+// let name: string = "Ratul";
+// let age: number = 345;
+// let isDeveloperf: boolean  = true;
+// let country:string= "BD";
+// const numbers: number[] = [10, 20, 30];
+// const names: string[] = ["Riadul", "Alex", "John"];
+// const statuses: boolean[] = [true, false, true];
+// const scores:number[] = [345,345,345,4534,54];
+// const friends:string[] = ["fdsfdsf", 'fdsfds0', 'sfdsf4', 'f4rewf'];
+// const loggedIn:boolean[] = [true, false, true, false];
+// const user5: {
+//   name: string;
+//   age: number;
+// } = {
+//   name: "Riadul",
+//   age: 22
+// };
+// const user2: {
+//   name: string;
+//   age: number;
+//   email?: string;
+// } = {
+//   name: "Riadul",
+//   age: 22
+// };
+// let role: "admin" | "user" | "moderator";
+// const userObj:{
+//     name: string,
+//     age: number,
+//     role:"admin",
+//     email?: string
+// } = {
+//     name:"rarul",
+//     age: 434,
+//     role:"admin",
+// }
+// let user: string[] = ["Riadul", "Developer"];
+// let user: [string, number] = ["Riadul", 22];
+// const tupleLet:[string, number, boolean] = ["apple", 34, true];
+// function add(a: number, b: number): number {
+//   return a + b;
+// }
+// function greet(name: string): string {
+//   return `Hello ${name}`;
+// }
+// function isAdult(age: number): boolean {
+//   return age >= 18;
+// }
+// function multiply(a:number, b:number): number {
+//     return a + b
+// }
+// function getFullName(firstName:string, lastName:string):string{
+//     return `${firstName} ${lastName}`
+// }
+// function isEven(number:number):boolean{
+//     return true
+// }
+// const studentsAll:{
+//     name:string,
+//     age:number,
+//     isStudent:boolean,
+//     skills:string[],
+//     role: "student",
+//     email?: string
+// } = {
+//     name: "Ratul",
+//     age:345,
+//     isStudent:true,
+//     skills:["html", "css", "js", "ts"],
+//     role:"student",
+// }
+// function getStudentInfo(student:{
+//     name:string,
+//     age:number,
+//     isStudent:boolean,
+//     skills:string[],
+//     role: "student",
+//     email?: string
+// }):string{
+// return `${student.name}, ${student.age} ${student.role}`
+// }
+// let name:string = "rakin";
+// let age:number = 34;
+// let isDeveloper:boolean = true;
+// let country:string = "Dv"

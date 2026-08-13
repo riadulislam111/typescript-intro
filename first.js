55fbdc6b1a520b41e5e869ddf603db317890439a
riadulislam111 <riadulislam.bd.pabna@gmail.com>
2026-08-13 22:59:42 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,5 @@
+let products = "sunglass";
+products = 45;
+products = true;
+products = ["book", "pen", "paper"]
+console.log(products);
---
+++ items.js
@@ -0,0 +1,15 @@
+"use strict";
+let items = 'laptop';
+// items = 120;
+// items = false;
+let students = ["Alice", "Bob", "Charlie"];
+let user = {
+    name: "Ratul",
+    age: 32,
+    id: true
+};
+let numbers = [34, 535, 345, 6346, 453, 345];
+console.log(items);
+console.log(students);
+console.log(user);
+console.log(numbers);
